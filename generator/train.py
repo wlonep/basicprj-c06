0906d0350f9ba4af0de382198225110d810231e3
Jiwon Park <frin6189@gmail.com>
2025-03-19 18:58:57 +0900

    Edited file structure

---
+++ generator/train.py
@@ -0,0 +1,78 @@
+import os
+import openpyxl
+from datetime import datetime
+
+
+def calculate_fee(start_time, end_time):
+    """
+    기본 운임 계산 함수
+    수식: 운행시간(분) * 250
+    """
+    try:
+        start = datetime.strptime(start_time, "%H%M")
+        end = datetime.strptime(end_time, "%H%M")
+        duration = (end - start).seconds / 60  # 분 단위 변환
+        return int(duration * 250)
+    except:
+        return 10000  # 기본값
+
+
+def main(sheet_data, header, dir_path):
+    for row in sheet_data:
+        train_id = row[0].value
+        # train_type = row[1].value.replace('KTX-', '')
+        # if train_type == "산천":
+        #     car_count = 8
+        #     seat_count = 363
+        # elif train_type == "청룡":
+        #     car_count = 8
+        #     seat_count = 515
+        # else:
+        #     train_type = 'A'
+        #     car_count = 18
+        #     seat_count = 935
+
+        stations = []
+        stop_time = []
+        cell_count = 0
+        # if row[20].value == "매일":
+        #     operation = "월,화,수,목,금,토,일"
+        # else:
+        #     operation = ','.join(list(row[20].value))
+
+        for cell in row[2:20]:
+            if cell.value.strftime("%H%M") == "0000":
+                pass
+            else:
+                stations.append(header[0][cell_count].value)
+                stop_time.append(cell.value.strftime("%H%M"))
+            cell_count += 1
+
+        train_data = f"""TRAIN_ID={train_id}
+STATION={','.join(stations)}
+FEE={calculate_fee(stop_time[0], stop_time[-1])}
+BASE_FEE=7000
+BOOKED="""
+        file_path = os.path.join(dir_path, f"KTX-{train_id}.txt")
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(train_data)
+
+
+wb = openpyxl.load_workbook("train_data.xlsx")
+
+# 경부선
+sheet = wb["경부선"]
+upward_sheet = sheet['B11':'V82']
+upward_header = sheet['D8':'U8']
+upward_dir = '../src/train/upward'
+downward_sheet = sheet['X11':'AR86']
+downward_header = sheet['Z8':'AQ8']
+downward_dir = '../src/train/downward'
+
+if not os.path.exists(upward_dir):
+    os.makedirs(upward_dir)
+if not os.path.exists(downward_dir):
+    os.makedirs(downward_dir)
+
+main(upward_sheet, upward_header, upward_dir)
+main(downward_sheet, downward_header, downward_dir)
